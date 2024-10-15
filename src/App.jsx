87ed7ee442082aa1a9186784fcf8b37912734866
Hamada Sayed <110072418+hamadasayedmo@users.noreply.github.com>
2024-10-15 16:48:19 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,10 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  
-  const [name, setName] = useState("Hamada");
-  const [number, setNumber] = useState(0);
-  const [age, setAge] = useState(18);
-  
 
   return (
     <div>
-      {/* Change Name */}
-      <h4>My Name Is {name}</h4>
-      <button onClick={ () => setName("Mede")}>Chang </button>
-
-      {/* +  , - number */}
-      <h4>{number}</h4>
-      <button
-        onClick={() => {
-          {
-            setNumber(number - 1);
-          }
-        }}
-      >
-        -
-      </button>
-      <button
-        onClick={() => {
-          {
-            setNumber(number + 1);
-          }
-        }}
-      >
-        +
-      </button>
-
-      {/* Change Age */}
-      <h4>My Age Is {age}</h4>
-      <button onClick={ () => setAge(21)}>Chang </button>
-
+      <h1>Create React App using Vite</h1>
     </div>
   );
 }
